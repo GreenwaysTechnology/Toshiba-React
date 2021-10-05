1973fcbd8ade8752a2dbea5b3b99423b92254162
Subramanian Murugan <sasubramanian_md@hotmail.com>
2021-10-05 17:07:38 +0530

    props,state,vdom,container and presentational

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,36 +1,37 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 
+class Counter extends React.Component {
 
-const DescriptionList = props => {
-    return <dl>
-        {props.items.map((item, index) => {
-            return <Fragment key={index}>
-                <dt>
-                    {item.term}
-                </dt>
-                <dd>
-                    {item.description}
-                </dd>
-            </Fragment>
-        })}
-    </dl>
+    state = {
+        counter: 10
+    };
+
+    //listener
+    onIncrement = () => {
+        let counter = this.state.counter + 1;
+        this.setState({ counter });
+    }
+
+    render() {
+        return <div className="container">
+        <CounterUI counter={this.state.counter} onIncrement={this.onIncrement}  />
+        </div>
+    }
 }
 
-const items = [{
-    term: 'coffee',
-    description: 'Cold Coffee'
-},
-{
-    term: 'Milk',
-    description: 'Milk has lot of protins'
-},
-]
-
-export const App = () => {
-    return <div className="container">
-        <DescriptionList items={items} />
-    </div>
+//presentational component
+const CounterUI = props => {
+    return <>
+        <h1>Counter App-Using State and Event Handling</h1>
+        <h2>Counter : {props.counter}</h2>
+        <button onClick={props.onIncrement} className="btn btn-success">+</button>
+    </>
 }
+
+
+
+const App = () => <Counter />
+
 ReactDOM.render(<App />, document.getElementById('root'));
