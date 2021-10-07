fee844d0bf71efbb8e858b3606d82eb1df0ff838
Subramanian Murugan <sasubramanian_md@hotmail.com>
2021-10-07 16:59:18 +0530

    react hooks,cmp life cycles,ajax

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,37 +1,64 @@
-import React from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 
-class Counter extends React.Component {
+//using side effects
+//Side effects using old react style
+// class Customer extends Component {
 
-    state = {
-        counter: 10
-    };
 
-    //listener
-    onIncrement = () => {
-        let counter = this.state.counter + 1;
-        this.setState({ counter });
+//     state = {
+//           screen: 'Customer Manager App'
+//     }
+//     add = () => {
+//           this.setState({ screen: 'Customer Manager App - Add ' })
+//     }
+//     render() {
+//           return <div>
+//                 <h1>Customer Manager App: using Side effects</h1>
+//                 <button onClick={this.add} >Add</button>
+//           </div>
+//     }
+//     //side effeects with inital / mount cycle
+//     componentDidMount() {
+//           //imperative dom
+//           document.title = this.state.screen;
+//     }
+//     //side effects with update cycle
+//     componentDidUpdate() {
+//           document.title = this.state.screen;
+//     }
+//componentWillUnMount(){
+//resourceclean up activit
+//}
+// }
+
+const Customer = props => {
+    const [screen, setScreen] = useState('Customer Manager App');
+    //componentDidMount && componentDidUpdate
+    useEffect(() => {
+        console.log('useEffect is called ', screen)
+        document.title = screen;
+
+        //componentWillUnMount- clean upcode
+        return () => {
+            //clean upcode 
+            console.log('component is destroyed')
+        };
+    })
+    //listener function to change the state
+    const changeTitle = () => {
+        setScreen("Customer Manager App - Add")
     }
 
-    render() {
-        return <div className="container">
-        <CounterUI counter={this.state.counter} onIncrement={this.onIncrement}  />
-        </div>
-    }
-}
-
-//presentational component
-const CounterUI = props => {
-    return <>
-        <h1>Counter App-Using State and Event Handling</h1>
-        <h2>Counter : {props.counter}</h2>
-        <button onClick={props.onIncrement} className="btn btn-success">+</button>
-    </>
+    return <div>
+        <h1>Customer Manager App: using Side effects</h1>
+        <button onClick={changeTitle} >ChangeTitle</button>
+    </div>
 }
 
 
 
-const App = () => <Counter />
+const App = () => <Customer />
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
