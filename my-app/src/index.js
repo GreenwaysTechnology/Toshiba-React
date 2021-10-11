a1240f014f3649a9fa614f5b47317cc2673ffe53
Subramanian Murugan <sasubramanian_md@hotmail.com>
2021-10-11 17:21:08 +0530

    advanced state management

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,67 +1,59 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import produce from "immer";
 
-//components 
-const Home = props => {
-    return <div>
-        <h2>Declarative</h2>
-        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
-            Declarative views make your code more predictable and easier to debug.
-        </p>
-    </div>
+function updateProfile(profile, city) {
+    // return { ...profile, city: city }
+    return produce(profile, draft => {
+        //mutable version of immutable
+        draft.city = city;
+    })
 }
 
-const Blog = props => {
+//inital state
+const profile = {
+    id: 1,
+    name: 'Subramanian',
+    city: 'chenai'
+};
+console.log(profile)
+const updatedProfile = updateProfile(profile, 'Coimbatore');
 
-    useEffect(() => {
-        document.title = 'Blog'
+console.log(updatedProfile)
 
-        //componentwillUnMount
-        return () => {
-            console.log('Component has removed')
-        }
+console.log('Same object(profile===updatedProfile)', profile === updatedProfile)
+/////////////////////////////////////////////////.................
+
+
+function updateCustomer(customer, mobileno) {
+
+    // return {
+    //     ...customer,
+    //     contact: {
+    //         ...customer.contact,
+    //         communication: {
+    //             ...customer.contact.communication,
+    //             mobileno: mobileno
+    //         }
+    //     }
+    // }
+    return produce(customer, draft => {
+        draft.contact.communication.mobileno = mobileno
     });
 
-    return <div>
-        <h1>The Plan for React 18</h1>
-        <p>The React team is excited to share a few updates:
-
-            We’ve started work on the React 18 release, which will be our next major version.
-            We’ve created a Working Group to prepare the community for gradual adoption of new features in React 18.
-            We’ve published a React 18 Alpha so that library authors can try it and provide feedback.
-            These updates are primarily aimed at maintainers of third-party libraries. If you’re learning, teaching, or using React to build user-facing applications, you can safely ignore this post. But you are welcome to follow the discussions in the React 18 Working Group if you’re curious!
-        </p>
-    </div>
 }
 
-const App = props => {
-    return <div className="container">
-        <h1>Single Page React Application</h1>
-        <BrowserRouter>
-            {/**Menus */}
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Blog</Link>
-                    </li>
-                </ul>
-            </nav>
-            {/**Router Rules : based on navigation what should i return */}
-            <Switch>
-                <Route path="/blog">
-                    <Blog />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    </div>
+let customer = {
+    id: 1,
+    name: 'Subramanian',
+    contact: {
+        address: {
+            city: 'Coimbatore'
+        },
+        communication: {
+            mobileno: '9000000000'
+        }
+    }
 }
-
-ReactDOM.render(<App />, document.getElementById('root'))
+console.log(customer);
+const updatedCustomer = updateCustomer(customer, '9003706368')
+console.log(updatedCustomer)
+console.log("Same Object (state===newState) ? =>", customer === updatedCustomer);
